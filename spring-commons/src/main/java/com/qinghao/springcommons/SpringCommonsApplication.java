aa0aeca265ea9248f7a34ae200868ac3a382cdc5
qinghao <qinghao@2dfire.com>
2018-01-27 12:08:35 +0800

    添加工程

---
+++ spring-commons/src/main/java/com/qinghao/springcommons/SpringCommonsApplication.java
@@ -0,0 +1,12 @@
+package com.qinghao.springcommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommonsApplication.class, args);
+	}
+}
